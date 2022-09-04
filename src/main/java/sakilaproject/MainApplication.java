6c72cbe9f140e57282dc277b983f7b975954f8c0
Norbs <norbertbobila12@gmail.com>
2022-09-04 18:45:46 +0800

    -a

--- src/main/java/sakilaproject/MainApplication.java
+++ src/main/java/sakilaproject/MainApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Hello");
         SpringApplication.run(MainApplication.class,args);
     }
 
