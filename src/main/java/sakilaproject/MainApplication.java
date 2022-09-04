c2df0d94af581a8a2eb2d2ec860ec9fcf36cd5a6
Norbs <norbertbobila12@gmail.com>
2022-09-04 18:35:10 +0800

    initialize

---
+++ src/main/java/sakilaproject/MainApplication.java
@@ -0,0 +1,13 @@
+package sakilaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println();
+        SpringApplication.run(MainApplication.class,args);
+    }
+
+}
